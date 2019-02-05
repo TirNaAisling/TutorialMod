881308f8d2fcac82b502d991955cb65a840820f3
R Marsh <rodtmarsh@gmail.com>
2019-02-05 13:58:49 +0000

    Update

--- src/main/java/craftlabs/tutorialmod/util/Reference.java
+++ src/main/java/craftlabs/tutorialmod/util/Reference.java
@@ -5,6 +5,6 @@ public class Reference {
 
 	public static final String NAME = "Tutorial Mod";
 	public static final String VERSION = "0.1 - MC 1.11.2";
-	public static final String CLIENT_PROXY = "craftlab.tutorialmod.proxy.ClientProxy";
-	public static final String COMMONPROXY = "craftlab.tutorialmod.proxy.CommonProxy";
+	public static final String CLIENT_PROXY = "craftlabs.tutorialmod.proxy.ClientProxy";
+	public static final String COMMONPROXY = "craftlabs.tutorialmod.proxy.CommonProxy";
 }
